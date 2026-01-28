b86cd587f4f7adebfbe9c2c1ccd9bf51caa16626
ANANDHAKUMAR <50989844+happyanand@users.noreply.github.com>
2026-01-28 12:43:17 +0530

    Add files via upload

---
+++ Anand.java
@@ -0,0 +1,10 @@
+class a
+{
+ public static void main(String args[])
+ {
+  for (int i=1;i<=5;i++)
+  {   
+   System.out.println("HI I am ANANDHAKUMAR\n");
+  }
+ }
+}
---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Sample
+{
+ public static void main(String b[]) //args is b also execute
+ {
+  //System.out.println(1/0);  Run time Error
+  //System.out.println("'");    //O/P: ' 
+  //System.out.print("anand");   
+  //System.out.print(60);          //o/p: anand60
+  System.out.print("1/0");
+ }
+}
